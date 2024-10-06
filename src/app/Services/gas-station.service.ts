3849a13ad73ae3fbc6137563ea0f8b094ac9b0b4
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-10-06 13:44:17 +0200

    servicio para conectarse a la base de datos hecho

--- src/app/Services/gas-station.service.ts
+++ src/app/Services/gas-station.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { GasStationDTO } from '../Models/gas-station.dto';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GasStationService {
 
-  constructor() { }
+  private urlGasolinerasApi: string;
+
+  constructor(private http: HttpClient) {
+    this.urlGasolinerasApi = 'http://localhost:3000/gasolineras';
+   }
+
+   getGasStationByLocation(location:  string): Observable<GasStationDTO[]>{    
+    return this.http.get<GasStationDTO[]>(this.urlGasolinerasApi + '/' + location);
+   }
 }

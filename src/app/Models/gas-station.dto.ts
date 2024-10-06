026584fda6ee2f3aee1e20fb0f19cdef8a4834ac
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-10-06 13:42:41 +0200

    modelos de los datos creados

---
+++ src/app/Models/gas-station.dto.ts
@@ -0,0 +1,14 @@
+export class GasStationDTO {
+    id!: number;
+    localidad!: string;
+    direccion!: string;
+    longitud!: string;
+    latitud!: string;
+    fechadatos!: string;
+    preciogasolina95!: number;
+    preciogasolina98!: number;
+    preciodiesel!: number;
+    preciodieselpremium!: number;
+    marca!: string;
+    horario!: string; 
+}

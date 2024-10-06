ffdbd65e76d3730d4f6a100250c6737982822c11
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-10-06 13:45:41 +0200

    modulos añadidos y nuevo path por defecto modificado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './Components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GasStationListComponent } from './Components/gas-station-list/gas-station-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { SearchFieldComponent } from './Components/search-field/search-field.component';
+import { HomeComponent } from './Components/home/home.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { HeaderComponent } from './Components/header/header.component';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    GasStationListComponent
+    GasStationListComponent,
+    SearchFieldComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

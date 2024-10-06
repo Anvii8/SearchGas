21e14a6affb288dffbfe9aeccfd98d32dbda967b
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-10-06 13:43:48 +0200

    componente para obtener las gasolineras del servicio 75% hecho

--- src/app/Components/gas-station-list/gas-station-list.component.ts
+++ src/app/Components/gas-station-list/gas-station-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GasStationDTO } from 'src/app/Models/gas-station.dto';
+import { GasStationService } from 'src/app/Services/gas-station.service';
 
 @Component({
   selector: 'app-gas-station-list',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class GasStationListComponent {
 
+  gasStations!: GasStationDTO[];
+
+  @Input() location!: string;
+
+  constructor(private gasStationService: GasStationService){}
+
+  /*ngOnInit(): void {
+    this.getGasStationsbyLocation('Martorell');
+  }*/
+
+  ngOnChanges() {
+    if (this.location) {
+      this.getGasStationsbyLocation(this.location);
+    }
+  }
+
+  getGasStationsbyLocation(location: string): void{
+    this.gasStationService.getGasStationByLocation(location).subscribe(
+      (data) => {
+        this.gasStations = data;
+      },
+      (error) => {
+        console.error('Error al obtener las gasolineras de la ubicación', error);
+      }
+    )
+  }
+
 }
+

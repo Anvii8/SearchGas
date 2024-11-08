53e024a60c318bd75b4755bc73c7c39545c50b01
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-11-08 21:43:46 +0100

    código añadido de la funcionalidad de lista de gasolineras y mapa

--- src/app/Components/gas-station-list/gas-station-list.component.ts
+++ src/app/Components/gas-station-list/gas-station-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { GasStationDTO } from 'src/app/Models/gas-station.dto';
 import { GasStationService } from 'src/app/Services/gas-station.service';
 
@@ -9,31 +9,67 @@ import { GasStationService } from 'src/app/Services/gas-station.service';
 })
 export class GasStationListComponent {
 
-  gasStations!: GasStationDTO[];
+  gasStations: GasStationDTO[] = [];
+  isSearched: boolean = false;
+  message: string = '';
+  isLoading: boolean = false;
 
   @Input() location!: string;
+  @Output() gasStationsListReceived = new EventEmitter<GasStationDTO[]>();
 
   constructor(private gasStationService: GasStationService){}
-
-  /*ngOnInit(): void {
+/*
+  ngOnInit(): void {
     this.getGasStationsbyLocation('Martorell');
   }*/
 
   ngOnChanges() {
     if (this.location) {
+      this.location = this.formatLocation(this.location);
       this.getGasStationsbyLocation(this.location);
     }
   }
 
   getGasStationsbyLocation(location: string): void{
+    this.isLoading = true;
     this.gasStationService.getGasStationByLocation(location).subscribe(
       (data) => {
-        this.gasStations = data;
+        this.isSearched = true;
+
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 500);
+        
+        if(data.length === 0){
+          this.message = 'No se han encontrado gasolineras en esta población. Por favor, prueba con otra población.';
+          this.gasStations = [];
+          this.gasStationsListReceived.emit(this.gasStations);
+        }
+        else{
+          this.gasStations = data;
+          this.gasStationsListReceived.emit(this.gasStations);
+          this.message = '';
+        }
       },
       (error) => {
-        console.error('Error al obtener las gasolineras de la ubicación', error);
+        console.error('Error al obtener las gasolineras de la población', error);
+        this.message = 'Hubo un error al obtener las gasolineras. Por favor, inténtalo de nuevo más tarde.';
+        this.isLoading = false;
       }
     )
+  }
+
+  formatLocation(location: string): string {
+    return location.charAt(0).toUpperCase() + location.slice(1).toLowerCase();
+  }
+
+  getFuelPrices(gasStation: GasStationDTO) {
+    return [
+        { type: 'Diesel', price: gasStation.preciodiesel },
+        { type: 'Diesel Premium', price: gasStation.preciodieselpremium },
+        { type: 'Gasolina 95', price: gasStation.preciogasolina95 },
+        { type: 'Gasolina 98', price: gasStation.preciogasolina98 }
+    ].filter(fuel => fuel.price != 0.000);
   }
 
 }

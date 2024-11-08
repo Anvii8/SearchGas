3282d372fe3c820b2ec4ae6446a27c71915073d6
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-11-08 21:39:46 +0100

    modificaciones visuales de código

--- src/app/Components/search-field/search-field.component.ts
+++ src/app/Components/search-field/search-field.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { GasStationListComponent } from '../gas-station-list/gas-station-list.component';
 
 @Component({
   selector: 'app-search-field',
@@ -11,14 +10,11 @@ export class SearchFieldComponent {
   locationFormControl = new FormControl('', [Validators.required]);
 
   @Output() locationSelected = new EventEmitter<string>();
-
-  //constructor(private gasStationComponent: GasStationListComponent){}
-
+  
   onSubmit() {
     if (this.locationFormControl.valid) {
       const location = this.locationFormControl.value;
       this.locationSelected.emit(location!);
-
     }
   }
 }

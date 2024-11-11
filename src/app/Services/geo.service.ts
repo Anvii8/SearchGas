ea469b301312e56e7b2f6f8564db13019959b900
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-11-11 20:35:25 +0100

    nueva llamada a la api

--- src/app/Services/geo.service.ts
+++ src/app/Services/geo.service.ts
@@ -7,10 +7,17 @@ import { Observable } from 'rxjs';
 })
 export class GeoService {
   private apiUrl = 'https://nominatim.openstreetmap.org/search?format=json&q=';
+  private reverseApiUrl = 'https://nominatim.openstreetmap.org/reverse?format=json';
 
   constructor(private http: HttpClient) {}
 
   getCoordinates(location: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}${encodeURIComponent(location)}`);
+  }
+
+  getLocationFromCoordinates(coordinates: [number, number]): Observable<any> {
+    const latitude: number = coordinates[0];
+    const longitude: number = coordinates[1];
+    return this.http.get<any>(`${this.reverseApiUrl}&lat=${latitude}&lon=${longitude}`);
   }
 }

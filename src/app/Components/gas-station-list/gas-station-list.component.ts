2766ae4a703c7bb2ad176cc5969b85ab62c39dc0
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-11-15 20:52:18 +0100

    funcionalidad filtros empezada

--- src/app/Components/gas-station-list/gas-station-list.component.ts
+++ src/app/Components/gas-station-list/gas-station-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 import { GasStationDTO } from 'src/app/Models/gas-station.dto';
 import { GasStationService } from 'src/app/Services/gas-station.service';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-gas-station-list',
@@ -10,27 +12,56 @@ import { GasStationService } from 'src/app/Services/gas-station.service';
 export class GasStationListComponent {
 
   gasStations: GasStationDTO[] = [];
+  sortedGasStations: GasStationDTO[] = [];
   isSearched: boolean = false;
   message: string = '';
   isLoading: boolean = false;
+  isAuthenticated: boolean = false;
   locationIndication: boolean = false;
 
   @Input() location!: string;
   @Input() fuel!: string[];
   @Output() gasStationsListReceived = new EventEmitter<GasStationDTO[]>();
 
-  constructor(private gasStationService: GasStationService){}
+  displayedColumns: string[] = ['marca', 'direccion', 'localidad', 'horario', 'fecha'];
 
-  ngOnChanges() {
+  fuelMapping: { [key: string]: string } = {
+    "Gasolina 95": "gasolina95",
+    "Gasolina 98": "gasolina98",
+    "Diesel": "diesel",
+    "Diesel Premium": "dieselPremium",
+  };
+
+  fuelPriceMapping: { [key: string]: string } = {
+    "Gasolina 95": "preciogasolina95",
+    "Gasolina 98": "preciogasolina98",
+    "Diesel": "preciodiesel",
+    "Diesel Premium": "preciodieselpremium",
+  };
+
+  constructor(private gasStationService: GasStationService, private userService: UserService){
+    this.isAuthenticated = this.userService.isAuthenticated();
+  }
+
+  ngOnChanges():void {
     if (this.location) {
       this.location = this.formatLocation(this.location);
       if(this.location && this.fuel){
         this.getGasStationsByLocationAndFuel(this.location, this.fuel);
+        this.updateDisplayedColumns();
       }
       else{
         this.getGasStationsbyLocation(this.location);
+        this.displayedColumns = ['marca', 'direccion', 'localidad', 'horario', 'diesel', 'dieselPremium', 'gasolina95', 'gasolina98', 'fecha'];
       }
     }
+  }
+
+  updateDisplayedColumns(): void {
+    const fuelColumns = this.fuel
+      .map(fuelType => this.fuelMapping[fuelType])
+      .filter(column => column);
+    this.displayedColumns = ['marca', 'direccion', 'localidad', 'horario', ...fuelColumns, 'fecha'];
   }
 
   getGasStationsbyLocation(location: string): void{
@@ -47,13 +78,13 @@ export class GasStationListComponent {
         if(data.length === 0){
           this.message = 'No se han encontrado gasolineras en esta población. Por favor, pruebe con otra población.';
           this.gasStations = [];
-          this.gasStationsListReceived.emit(this.gasStations);
         }
         else{
           this.gasStations = data;
-          this.gasStationsListReceived.emit(this.gasStations);
+          this.sortedGasStations = this.gasStations.slice();
           this.message = '';
         }
+        this.gasStationsListReceived.emit(this.gasStations);
       },
       (error) => {
         console.error('Error al obtener las gasolineras de la población', error);
@@ -65,15 +96,6 @@ export class GasStationListComponent {
 
   formatLocation(location: string): string {
     return location.charAt(0).toUpperCase() + location.slice(1).toLowerCase();
-  }
-
-  getFuelPrices(gasStation: GasStationDTO) {
-    return [
-        { type: 'Diesel', price: gasStation.preciodiesel },
-        { type: 'Diesel Premium', price: gasStation.preciodieselpremium },
-        { type: 'Gasolina 95', price: gasStation.preciogasolina95 },
-        { type: 'Gasolina 98', price: gasStation.preciogasolina98 }
-    ].filter(fuel => fuel.price != 0.000);
   }
 
   getGasStationsByLocationAndFuel(location: string, fuel: string[]): void{
@@ -90,13 +112,14 @@ export class GasStationListComponent {
         if(data.length === 0){
           this.message = 'No se han encontrado gasolineras en esta población. Por favor, pruebe con otra población y/o seleccione otro tipo de combustible.';
           this.gasStations = [];
-          this.gasStationsListReceived.emit(this.gasStations);
         }
         else{
           this.gasStations = data;
-          this.gasStationsListReceived.emit(this.gasStations);
+          this.sortedGasStations = this.gasStations.slice();
           this.message = '';
         }
+        this.gasStationsListReceived.emit(this.gasStations);
+
       },
       (error) => {
         console.error('Error al obtener las gasolineras de la población', error);
@@ -106,5 +129,33 @@ export class GasStationListComponent {
     )
   }
 
+  sortData(sort: Sort) {    
+    const data = this.gasStations.slice();
+    if (!sort.active || sort.direction === '') {
+      return;
+    }
+
+    this.sortedGasStations = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';      
+      switch (sort.active) {
+        case 'diesel':          
+          return this.compare(a.preciodiesel, b.preciodiesel, isAsc);
+        case 'dieselPremium':
+          return this.compare(a.preciodieselpremium, b.preciodieselpremium, isAsc);
+        case 'gasolina95':
+          return this.compare(a.preciogasolina95, b.preciogasolina95, isAsc);
+        case 'gasolina98':
+          return this.compare(a.preciogasolina98, b.preciogasolina98, isAsc);
+        default:
+          return 0;
+      }
+    });
+    this.gasStations = this.sortedGasStations;
+  }
+
+  private compare(a: number | string, b: number | string, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+  
 }
 

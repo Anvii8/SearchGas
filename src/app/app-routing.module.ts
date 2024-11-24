1a8b4196cd61efaca9ae5e8f08f472ee0b5c8d48
AlbertAndresVidueira <aandresvi@uoc.edu>
2024-11-24 17:35:39 +0100

    nueva ruta añadida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ import { RegisterComponent } from './Components/register/register.component';
 import { FavoritesComponent } from './Components/favorites/favorites.component';
 import { AuthGuard } from './Guards/auth.guard';
 import { GasStationDetailsComponent } from './Components/gas-station-details/gas-station-details.component';
+import { PrivacyInformationComponent } from './Components/privacy-information/privacy-information.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'aboutUs', component: PrivacyInformationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
